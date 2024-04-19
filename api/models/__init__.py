fc7b13d68057d4e0e118bd95926ef58ec1fd4076
Sachin Bisht <sachin.bisht@coloredcow.in>
2024-04-19 19:37:23 +0530

    Added user details based tables

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -2,4 +2,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+from .user_activities import *
+from .user_attributes import *
+from .user_flows import *
+from .user_indicator_responses import *
+from .users import *
 from .webhook_transaction_log import *
