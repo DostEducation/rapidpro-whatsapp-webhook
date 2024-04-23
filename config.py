0b74cebc468acc3288dfba9cb5bc33b14b45a702
Sachin Bisht <sachin.bisht@coloredcow.in>
2024-04-23 17:51:11 +0530

    Using db connection uri instead of creating it

--- config.py
+++ config.py
@@ -4,39 +4,6 @@ import os
 
 FLASK_APP = os.environ.get("FLASK_APP", "development")
 
-if FLASK_APP == "development":
-    from dotenv import load_dotenv
-
-    load_dotenv()
-
-
-# Database configuration
-POSTGRES = {
-    "user": os.environ.get("DB_USER"),
-    "password": os.environ.get("DB_PASSWORD"),
-    "database": os.environ.get("DB_NAME"),
-    "host": os.environ.get("DB_HOST"),
-    "port": os.environ.get("DB_PORT"),
-    "conn_str": os.environ.get("CONNECTION_NAME"),
-}
-
-SQLALCHEMY_DATABASE_URI = (
-    "postgresql://%(user)s:%(password)s@%(host)s:%(port)s/%(database)s"
-    % POSTGRES
-)
-
-# For socket based connection
-if FLASK_APP in ("production", "staging"):
-    SQLALCHEMY_DATABASE_URI = (
-        "postgresql://%(user)s:%(password)s@/%(database)s?host=%(conn_str)s/"
-        % POSTGRES
-    )
-
-github_action_db_url = os.environ.get(
-    "SQLALCHEMY_DATABASE_URI_GITHUB_ACTION", None
-)
-
-if github_action_db_url:
-    SQLALCHEMY_DATABASE_URI = github_action_db_url
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", None)
 
 LOGGING_LEVEL = os.environ.get("LOGGING_LEVEL", "DEBUG")
