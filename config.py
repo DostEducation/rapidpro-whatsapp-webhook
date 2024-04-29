97f8b3df60f2a6ce1cfb5d383aa817b08ddc14c0
Sachin Bisht <sachin.bisht@coloredcow.in>
2024-04-29 14:47:57 +0530

    Corrected environment based db connection

--- config.py
+++ config.py
@@ -2,7 +2,12 @@
 
 import os
 
-FLASK_APP = os.environ.get("FLASK_APP", "development")
+FLASK_ENV = os.environ.get("FLASK_ENV", "development")
+
+if FLASK_ENV == "development":
+    from dotenv import load_dotenv
+
+    load_dotenv()
 
 SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", None)
 
