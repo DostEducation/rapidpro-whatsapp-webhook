5a5f9cf122a80625f7e2217b4fe600961c452883
Sachin Bisht <sachin.bisht@coloredcow.in>
2024-05-08 19:10:18 +0530

    Corrected the datatype for the timestamp columns

--- api/helpers/common_helper.py
+++ api/helpers/common_helper.py
@@ -1,10 +1,10 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 
 def get_ist_timestamp() -> datetime:
-    # ist_offset = timedelta(hours=5, minutes=30)
+    ist_offset = timedelta(hours=5, minutes=30)
     utc_now = datetime.now(timezone.utc)
-    return utc_now
+    return utc_now + ist_offset
 
 
 def check_activity_key(activity_key: str, keyword: str, status: str):
