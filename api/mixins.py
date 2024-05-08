32b7ea14436702052d3f8aedd31e542cc2f1d1ff
Sachin Bisht <sachin.bisht@coloredcow.in>
2024-05-08 19:28:15 +0530

    Renamed the variables

--- api/mixins.py
+++ api/mixins.py
@@ -1,14 +1,14 @@
 from api import db
 from api.helpers import common_helper
 
-ist_now = common_helper.get_current_utc_timestamp()
+time_now = common_helper.get_current_utc_timestamp()
 
 
 class TimestampMixin:
-    created_on = db.Column(db.DateTime(timezone=True), default=ist_now, nullable=False)
+    created_on = db.Column(db.DateTime(timezone=True), default=time_now, nullable=False)
     updated_on = db.Column(
         db.DateTime(timezone=True),
-        onupdate=ist_now,
-        default=ist_now,
+        onupdate=time_now,
+        default=time_now,
         nullable=False,
     )
