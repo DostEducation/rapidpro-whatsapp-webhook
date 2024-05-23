54c3024a8f2a3aa4893968e9294e6155223999ec
Sachin Bisht <sachin.bisht@coloredcow.in>
2024-05-23 16:27:20 +0530

    Added feature to maintain user attributes

--- api/models/user_attributes.py
+++ api/models/user_attributes.py
@@ -1,8 +1,19 @@
+from flask_sqlalchemy.query import Query as BaseQuery
+
 from api import db
 from api.mixins import TimestampMixin
 
 
+class UserAttributesQuery(BaseQuery):
+    def get_existing_user_attributes(self, user_id):
+        user_attributes = self.filter(
+            UserAttributes.user_id == user_id,
+        ).all()
+        return {attribute.field_name: attribute for attribute in user_attributes}
+
+
 class UserAttributes(TimestampMixin, db.Model):
+    query_class = UserAttributesQuery
 
     __tablename__ = "user_attributes"
     id = db.Column(db.Integer, primary_key=True)
--- api/services/__init__.py
+++ api/services/__init__.py
@@ -1,5 +1,6 @@
 from .flow_run_log_service import *
 from .user_activities_service import *
+from .user_attribute_service import *
 from .user_creation_service import *
 from .user_indicator_response_service import *
 from .webhook_transaction_log_service import *
